6c2f3c344f2e97a948ac987b8139ee80368fcdf3
CedricMlg <malingre.cedric@gmail.com>
2023-01-30 18:16:19 +0100

    Edit data to add working performance chart

--- SportSee-App-Front/src/components/PerformanceChart/index.jsx
+++ SportSee-App-Front/src/components/PerformanceChart/index.jsx
@@ -9,14 +9,16 @@ import {
 
 function PerformanceChart(data) {
   const kind = data.props.kind;
-  const performance = data.props.data;
+  const dataPerf = data.props.data;
+  const performance = []
+  dataPerf.map((perf) => performance.push({ value: perf.value, kind: perf.kind.toString().replace(perf.kind, kind[perf.kind])}) )
   return (
     <div className="radar-chart">
       <RadarChart width={500} height={300} cx="50%" cy="50%" outerRadius="80%" data={performance}>
-        {/* <PolarGrid />
-        <PolarAngleAxis dataKey="subject" />
+        <PolarGrid />
+        <PolarAngleAxis dataKey="kind" />
         <PolarRadiusAxis />
-        <Radar dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} /> */}
+        <Radar dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
       </RadarChart>
     </div>
   );
