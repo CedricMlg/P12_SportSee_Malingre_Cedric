ff9e53d2f05fb222205a9dd2eb02c6c220978a78
CedricMlg <malingre.cedric@gmail.com>
2023-01-31 15:58:45 +0100

    Adapt score chart to wrong data key

--- SportSee-App-Front/src/components/ScoreChart/index.jsx
+++ SportSee-App-Front/src/components/ScoreChart/index.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { PieChart, Pie, Cell } from "recharts";
 
 function ScoreChart(data) {
-  const percent = data.props.todayScore * 100;
+  const userData = data.props;
+  if (userData.todayScore === undefined) {
+    userData.todayScore = userData.score;
+    delete userData.score;
+  }
+  const percent = userData.todayScore * 100;
   const missingPercent = 100 - percent;
   const score = [{ value: percent }, { value: missingPercent }];
   return (
