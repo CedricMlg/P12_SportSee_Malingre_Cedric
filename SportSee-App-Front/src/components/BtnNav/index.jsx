59e8ffdc8fefee9b7f2b9189353d17d505c14a8c
CedricMlg <malingre.cedric@gmail.com>
2023-02-01 17:11:44 +0100

    Add style to header/navbar place content porperly

--- SportSee-App-Front/src/components/BtnNav/index.jsx
+++ SportSee-App-Front/src/components/BtnNav/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BtnNav(props) {
     const svg = props.props
   return (
-    <button className='nav-button'><img src={svg} /></button>
+    <button className='btn-nav'><img src={svg} /></button>
   )
 }
 
