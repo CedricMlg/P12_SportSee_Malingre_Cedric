64fdc3ee5aa9c55ba0522792e28e37e441c2edd4
CedricMlg <malingre.cedric@gmail.com>
2023-02-02 13:32:55 +0100

    Add style to home page to choose between two users

--- SportSee-App-Front/src/pages/Home/index.jsx
+++ SportSee-App-Front/src/pages/Home/index.jsx
@@ -4,12 +4,8 @@ import { NavLink } from "react-router-dom";
 function Home() {
   return (
     <div className="home">
-      <NavLink to="/user/12">
-        <li>User12</li>
-      </NavLink>
-      <NavLink to="/user/18">
-        <li>User18</li>
-      </NavLink>
+      <NavLink to="/user/12">User12</NavLink>
+      <NavLink to="/user/18">User18</NavLink>
     </div>
   );
 }
